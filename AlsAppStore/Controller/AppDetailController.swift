08342971fcef300ed1cff382c4cc1cb5d29e0cf1
Allen Boynton <alboynton4@gmail.com>
2017-11-18 00:24:54 -0500

    Added RESTful API for parsing json

--- AlsAppStore/Controller/AppDetailController.swift
+++ AlsAppStore/Controller/AppDetailController.swift
@@ -11,10 +11,44 @@ import UIKit
 class AppDetailController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     private let headerId = "headerId"
+    private let cellId = "cellId"
     
     var app: App? {
         didSet {
             navigationItem.title = app?.Name
+            
+            if app?.Screenshots != nil {
+                return
+            }
+            
+            if let id = app?.Id {
+                if let file = Bundle.main.url(forResource: "alsAppStoreDetails\(id)", withExtension: "json") {
+                    URLSession.shared.dataTask(with: file) { (data, response, error) -> Void in
+                        
+                        guard let data = data else { return }
+                        
+                        if let error = error {
+                            print(error)
+                            return
+                        }
+                        
+                        do {
+                            let decoder = JSONDecoder()
+                            let appDetail = try decoder.decode(App.self, from: data)
+                            
+                            self.app = appDetail
+                            
+                            DispatchQueue.main.async(execute: { () -> Void in
+                                self.collectionView?.reloadData()
+                            })
+                            
+                        } catch let err {
+                            print(err)
+                        }
+                        
+                    }.resume()
+                }
+            }
         }
     }
     
@@ -25,6 +59,22 @@ class AppDetailController: UICollectionViewController, UICollectionViewDelegateF
         collectionView?.backgroundColor = .white
         
         collectionView?.register(AppDetailHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+        collectionView?.register(ScreenshotsCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ScreenshotsCell
+        cell.app = app
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {

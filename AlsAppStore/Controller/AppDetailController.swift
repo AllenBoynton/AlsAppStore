2517e92b03df96d60a7f5c26a35520aed1d5431b
Allen Boynton <alboynton4@gmail.com>
2017-11-18 12:26:57 -0500

    Finalized RESTful Json data parsing

--- AlsAppStore/Controller/AppDetailController.swift
+++ AlsAppStore/Controller/AppDetailController.swift
@@ -12,6 +12,7 @@ class AppDetailController: UICollectionViewController, UICollectionViewDelegateF
     
     private let headerId = "headerId"
     private let cellId = "cellId"
+    private let descriptionCellId = "descriptionCellId"
     
     var app: App? {
         didSet {
@@ -22,32 +23,32 @@ class AppDetailController: UICollectionViewController, UICollectionViewDelegateF
             }
             
             if let id = app?.Id {
-                if let file = Bundle.main.url(forResource: "alsAppStoreDetails\(id)", withExtension: "json") {
-                    URLSession.shared.dataTask(with: file) { (data, response, error) -> Void in
+                let urlString = "https://api.letsbuildthatapp.com/appstore/appdetail?id=\(id)"
+                URLSession.shared.dataTask(with: URL(string: urlString)!, completionHandler: {
+                    (data, response, error) -> Void in
+                    
+                    guard let data = data else { return }
+                    
+                    if let error = error {
+                        print(error)
+                        return
+                    }
+                    
+                    do {
+                        let decoder = JSONDecoder()
+                        let appDetail = try decoder.decode(App.self, from: data)
                         
-                        guard let data = data else { return }
+                        self.app = appDetail
                         
-                        if let error = error {
-                            print(error)
-                            return
-                        }
+                        DispatchQueue.main.async(execute: { () -> Void in
+                            self.collectionView?.reloadData()
+                        })
                         
-                        do {
-                            let decoder = JSONDecoder()
-                            let appDetail = try decoder.decode(App.self, from: data)
-                            
-                            self.app = appDetail
-                            
-                            DispatchQueue.main.async(execute: { () -> Void in
-                                self.collectionView?.reloadData()
-                            })
-                            
-                        } catch let err {
-                            print(err)
-                        }
-                        
-                    }.resume()
-                }
+                    } catch let err {
+                        print(err)
+                    }
+                    
+                }).resume()
             }
         }
     }
@@ -60,9 +61,38 @@ class AppDetailController: UICollectionViewController, UICollectionViewDelegateF
         
         collectionView?.register(AppDetailHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
         collectionView?.register(ScreenshotsCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView?.register(AppDescriptionCell.self, forCellWithReuseIdentifier: descriptionCellId)
+    }
+    
+    // Creates text and text style with spacing for entire textView. Prevents needing multiple labels/textV
+    private func descriptionAttributedText() -> NSAttributedString {
+        // Creates text attribute for first line section
+        let attributedText = NSMutableAttributedString(string: "Description\n", attributes: [NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 14)])
+        
+        // Creates paragraph spacing
+        let style = NSMutableParagraphStyle()
+        style.lineSpacing = 10
+        
+        // Creates the following text with text length
+        let range = NSMakeRange(0, attributedText.string.characters.count)
+        attributedText.addAttribute(NSAttributedStringKey.paragraphStyle, value: style, range: range)
+        
+        if let desc = app?.description {
+            attributedText.append(NSAttributedString(string: desc, attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 11), NSAttributedStringKey.foregroundColor: UIColor.darkGray]))
+        }
+        
+        return attributedText
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if indexPath.item == 1 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: descriptionCellId, for: indexPath) as! AppDescriptionCell
+            
+            cell.descriptionTextView.attributedText = descriptionAttributedText()
+            
+            return cell
+        }
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! ScreenshotsCell
         cell.app = app
         
@@ -70,10 +100,18 @@ class AppDetailController: UICollectionViewController, UICollectionViewDelegateF
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return 2
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // Creates proper spacing for height of section height
+        if indexPath.item == 1 {
+            let dummySize = CGSize(width: view.frame.width - 14 - 14, height: 1000)
+            let options = NSStringDrawingOptions.usesFontLeading.union(NSStringDrawingOptions.usesLineFragmentOrigin)
+            let rect = descriptionAttributedText().boundingRect(with: dummySize, options: options, context: nil)
+            return CGSize(width: view.frame.width, height: rect.height + 30)
+        }
         return CGSize(width: view.frame.width, height: 200)
     }
     
